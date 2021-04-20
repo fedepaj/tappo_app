468c47cdd90e23ca5d51079dcf7e0e57190a2526
Federico Paglioni <paglioni.federico@gmail.com>
2021-04-20 17:12:59 +0200

    Update eps.dart

--- tappo/lib/eps.dart
+++ tappo/lib/eps.dart
@@ -1,12 +1,12 @@
 final String ledStatus =
-    "https://76lrbps52e.execute-api.us-east-1.amazonaws.com/test/tank_1/led";
+    "https://<your endpoint address>.execute-api.us-east-1.amazonaws.com/test/tank_1/led";
 final String ledToggle =
-    "https://76lrbps52e.execute-api.us-east-1.amazonaws.com/test/tank_1/led/toggle";
+    "https://<your endpoint address>.execute-api.us-east-1.amazonaws.com/test/tank_1/led/toggle";
 final String buzzStatus =
-    "https://76lrbps52e.execute-api.us-east-1.amazonaws.com/test/tank_1/buzz";
+    "https://<your endpoint address>.execute-api.us-east-1.amazonaws.com/test/tank_1/buzz";
 final String buzzToggle =
-    "https://76lrbps52e.execute-api.us-east-1.amazonaws.com/test/tank_1/buzz/toggle";
+    "https://<your endpoint address>.execute-api.us-east-1.amazonaws.com/test/tank_1/buzz/toggle";
 final String getPerc =
-    "https://76lrbps52e.execute-api.us-east-1.amazonaws.com/test/tank_1/value";
+    "https://<your endpoint address>.execute-api.us-east-1.amazonaws.com/test/tank_1/value";
 final String getActive =
-    "https://76lrbps52e.execute-api.us-east-1.amazonaws.com/test/tank_1/active";
+    "https://<your endpoint address>.execute-api.us-east-1.amazonaws.com/test/tank_1/active";
